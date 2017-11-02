e80f65bcb194f102c10722811769d25f57bbc2e6
Joseph Wilk <joe@josephwilk.net>
2017-11-02 18:02:49 +0000

    Map input file to word hashed output file. Lookup providing map between hashed and word is not processed yet.

--- project.clj
+++ project.clj
@@ -3,4 +3,12 @@
   :url "https://github.com/MastodonC/kixi.anonymisation"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [clojure-stemmer "0.1.0"]
+                 [org.bouncycastle/bcprov-jdk15on "1.58"]
+                 [pandect "0.6.1"]]
+
+  :profiles {:dev {:dependencies [[midje "1.8.3"]]
+                   :plugins      [[lein-kibit "0.1.5"]
+                                  [lein-midje "3.2.1"]
+                                  [jonase/eastwood "0.2.5"]]}})
--- src/kixi/anonymisation.clj
+++ src/kixi/anonymisation.clj
@@ -1,1 +1,35 @@
-(ns kixi.anonymisation)
+(ns kixi.anonymisation
+  (require
+   [clojure-stemmer.porter.stemmer :as stemmer]
+   [pandect.algo.sha3-256 :as sha3]))
+
+(defn ts [] (quot (System/currentTimeMillis) 1000))
+
+(defn words->line [words]
+  (str (clojure.string/join " " words) "\n") )
+
+(defn line->words [line]
+  (let [words (clojure.string/split line #"\s+")]
+    (remove clojure.string/blank? words)))
+
+(defn hash [word]
+  (-> word
+      (str ts)
+      sha3/sha3-256))
+
+(defn anon-word [lookup word]
+  (let [root-word (stemmer/stemming word)]
+    (get lookup root-word (hash root-word))))
+
+(defn line->anon-line [line lookup]
+  (->> line
+       line->words
+       (map (partial anon-word lookup))
+       words->line))
+
+(defn anonymise [in-file out-file]
+  (let [lookup {}]
+    (with-open [reader (clojure.java.io/reader in-file)]
+      (with-open [writer (clojure.java.io/writer out-file)]
+        (doseq [line (line-seq reader)]
+          (.write writer (line->anon-line line lookup)))))))

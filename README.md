8e46e4c0510ffba640d4affd8b0e97e6b384f2a7
Joseph Wilk <joe@josephwilk.net>
2017-11-02 15:15:57 +0000

    :skull: Project skeleton.

---
+++ README.md
@@ -0,0 +1,8 @@
+# kixi.anonymisation
+
+## License
+
+Copyright © 2017 MastodonC
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject kixi.anonymisation "0.1.0-SNAPSHOT"
+  :description "Anonymisation"
+  :url "https://github.com/MastodonC/kixi.anonymisation"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])

92aa7f93b7a7113a5c49df4be49cf3cc1910e59f
Joseph Wilk <joe@josephwilk.net>
2017-11-03 16:24:47 +0000

    TODOs

--- test/kixi/unit/t_anonoymisation.clj
+++ test/kixi/unit/t_anonoymisation.clj
@@ -4,7 +4,7 @@
 
 (fact "it should convert each word to a hash"
   (let [hashed (anonymise-chunk "Curiouser and curiouser!")
-        hashed-words (line->words hashed)]
+        hashed-words (line->tokens hashed)]
 
     (count hashed-words) => 3
 
@@ -15,4 +15,14 @@
 (fact "it should use the same hash for the same stemmed word"
   (let [hashes (line->words (anonymise-chunk "Feeds a feed"))]
     (nth hashes 0) => (nth hashes 2))
+  )
+
+(future-fact "punctation should not affect word hashing"
+  (let [hashes (line->words (anonymise-chunk "Feeds a feed."))]
+    (nth hashes 0) => (nth hashes 2))
+  )
+
+(future-fact "punctation should be preserved"
+  (let [hashes (line->words (anonymise-chunk "Feeds a feed."))]
+    (last hashes) => ".")
   )

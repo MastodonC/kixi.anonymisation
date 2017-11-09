fa1f3cf4e1de95ee84228b8ef872ec7ebb947468
Joseph Wilk <joe@josephwilk.net>
2017-11-09 16:48:40 +0000

    Support running against directories.

--- src/kixi/anonymisation/hide.clj
+++ src/kixi/anonymisation/hide.clj
@@ -33,13 +33,14 @@
     {:lookup @lookup
      :content anon-chunk}))
 
-(defn- anonomise-file [in-file out-file]
-  (let [lookup (atom {})]
-     (with-open [reader (clojure.java.io/reader in-file)]
-       (with-open [writer (clojure.java.io/writer out-file)]
-         (doseq [line (line-seq reader)]
-           (.write writer (sentence->anon-sentence lookup line)))))
-     @lookup))
+(defn- anonomise-file
+  ([in-file out-file] (anonomise-file in-file out-file (atom {})))
+  ([in-file out-file lookup]
+   (with-open [reader (clojure.java.io/reader in-file)]
+     (with-open [writer (clojure.java.io/writer out-file)]
+       (doseq [line (line-seq reader)]
+         (.write writer (sentence->anon-sentence lookup line)))))
+   @lookup))
 
 (defn from-file
   ([in-file out-file]
@@ -50,3 +51,14 @@
          lookup-whitelisted (whitelist/from-file lookup whitelist-file)]
      (spit "lookup.edn"              (prn-str lookup))
      (spit "lookup.edn.whitelisted" (prn-str lookup-whitelisted)))))
+
+(defn from-files [in-dir out-dir whitelist-file]
+  (let [lookup (reduce (fn [lookup in-file]
+                         (let [out-file (clojure.string/replace in-file in-dir out-dir)
+                               new-lookup (anonomise-file in-file out-file lookup)]
+                           new-lookup))
+                       (atom {})
+                       (file-seq (clojure.java.io/file in-dir)))
+        lookup-whitelisted (whitelist/from-file lookup whitelist-file)]
+    (spit (str out-dir "/lookup.edn")             (prn-str lookup))
+    (spit (str out-dir "/lookup.edn.whitelisted") (prn-str lookup-whitelisted))))
